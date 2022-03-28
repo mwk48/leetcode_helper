11e06e7a3202ad8f0d56820480710be44533a20c
mwk48 <mwk0048@gmail.com>
2022-03-28 02:43:46 +0000

    modify some react-query options

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { QueryClient, QueryClientProvider } from "react-query";
 import Provider from './contexts/context';
-const queryClient = new QueryClient();
+
+const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnmount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: twentyFourHoursInMs,
+    },
+  },
+});
 
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
