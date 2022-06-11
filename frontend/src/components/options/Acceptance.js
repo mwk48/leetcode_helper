ee9a6fa2c7b9573f91c71fe3188979dee7706d33
mwk48 <mwk0048@gmail.com>
2022-06-11 22:50:26 +0800

    debounce acceptance

--- frontend/src/components/options/Acceptance.js
+++ frontend/src/components/options/Acceptance.js
@@ -1,15 +1,30 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 import Typography from "@mui/material/Typography";
 import { queryContext } from "../../contexts/context";
+import debounce from "lodash/debounce";
 
 
 const Acceptance = () => {
     const { state, changeAcceptance, changePage } = useContext(queryContext);
+
+    const callHttpRequest = (eventSrcDesc, newValue) => {
+        changeAcceptance(newValue);
+    };
+
+    const [stateDebounceCallHttpRequest] = useState(() =>
+        debounce(callHttpRequest, 100, {
+            leading: false,
+            trailing: true
+        })
+    );
+    const [tempAcceptance, setTempAcceptance] = useState(state.acceptance);
+
     const handleChangeAcceptance = (event) => {
-        changeAcceptance(event.target.value);
+        setTempAcceptance(event.target.value);
+        stateDebounceCallHttpRequest("", event.target.value);
         changePage(1);
     };
     return (
@@ -19,7 +34,7 @@ const Acceptance = () => {
                 aria-label="Always visible"
                 defaultValue={0}
                 step={1}
-                value={state.acceptance}
+                value={tempAcceptance}
                 onChange={handleChangeAcceptance}
                 valueLabelDisplay="auto"
                 data-testid="slider"
